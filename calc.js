1a1989e5dc52ad04e590cd28ccae196bda84bd6f
venkatesh tripathi <venkateshp.tripathi@gmail.com>
2020-09-13 21:40:31 +0530

    initial commit

---
+++ calc.js
@@ -0,0 +1,64 @@
+let comedy= document.getElementById("comedy");
+let thr =document.getElementById("thr");
+let mys =document.getElementById("mys");
+let sus =document.getElementById("sus");
+let rom =document.getElementById("rom");
+
+
+
+
+function comedytab(){
+    var a= [];
+   a[0]="https://www.imdb.com/title/tt2015381/?ref_=adv_li_tt";
+   a[1]="https://www.imdb.com/title/tt0120382/?ref_=adv_li_tt";
+   a[2]="https://www.imdb.com/title/tt1645170/?ref_=nv_sr_srsg_0";
+   a[3]="https://www.imdb.com/title/tt0443453/?ref_=tt_sims_tt";
+   a[4]="https://www.imdb.com/title/tt1637725/?ref_=tt_sims_tt";
+
+    window.open(a[Math.floor((Math.random() * 5))  ]);
+}
+
+function thrt(){
+    let a= [];
+   a[0]="https://www.imdb.com/title/tt0468569/?ref_=adv_li_tt";
+   a[1]="https://www.imdb.com/title/tt1375666/?ref_=adv_li_tt";
+   a[2]="https://www.imdb.com/title/tt0114369/?ref_=adv_li_tt";
+   a[3]="https://www.imdb.com/title/tt0407887/?ref_=adv_li_tt";
+   a[4]="https://www.imdb.com/title/tt1130884/?ref_=adv_li_tt";
+
+
+let g=Math.floor((Math.random() * 5)) ;
+// console.log(g);
+
+    window.open(a[g]);
+}
+function myst(){
+    var a= [];
+   a[0]="https://www.imdb.com/title/tt0482571/?ref_=adv_li_tt";
+   a[1]="https://www.imdb.com/title/tt0114814/?ref_=adv_li_tt";
+   a[2]="https://www.imdb.com/title/tt2267998/?ref_=adv_li_tt";
+   a[3]="https://www.imdb.com/title/tt0167404/?ref_=adv_li_tt";
+   a[4]="https://www.imdb.com/title/tt3460252/?ref_=adv_li_tt";
+
+    window.open(a[Math.floor((Math.random() * 5))  ]);
+}
+function sust(){
+    var a= [];
+    a[0]="https://www.imdb.com/title/tt5052448/?ref_=adv_li_tt";
+    a[1]="https://www.imdb.com/title/tt1396484/?ref_=adv_li_tt";
+    a[2]="https://www.imdb.com/title/tt1457767/?ref_=adv_li_tt";
+    a[3]="https://www.imdb.com/title/tt0081505/?ref_=adv_li_tt";
+    a[4]="https://www.imdb.com/title/tt7784604/?ref_=adv_li_tt";
+
+    window.open(a[Math.floor((Math.random() * 5))  ]);
+}
+function romt(){
+    var a= [];
+    a[0]="https://www.imdb.com/title/tt0338013/?ref_=adv_li_tt";
+    a[1]="https://www.imdb.com/title/tt0120338/?ref_=adv_li_tt";
+    a[2]="https://www.imdb.com/title/tt1045658/?ref_=adv_li_tt";
+    a[3]="https://www.imdb.com/title/tt1798709/?ref_=adv_li_tt";
+    a[4]="https://www.imdb.com/title/tt1343092/?ref_=adv_li_tt";
+
+    window.open(a[Math.floor((Math.random() * 5))  ]);
+}
